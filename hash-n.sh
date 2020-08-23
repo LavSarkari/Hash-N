2da5a3fab0f7220d41341454b18881c62ce77dba
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-08-23 12:55:05 +0530

    Create hash-n.sh

---
+++ hash-n.sh
@@ -0,0 +1,9 @@
+pkg install figlet toilet python curl -y
+echo This Script Is Created By LavSarkari
+clear
+figlet Hash-N
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border LavSarkari
+echo " "
+echo -e "\e[1;34m Your requriments has been installed \e[1;32m"
+echo -e "\e[1;32m run it by typing python h-n.py \e[1;32m"
